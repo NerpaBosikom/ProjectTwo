11345735243aed0afa48d072686f08225be325ca
Svetlana <bosekom@bk.ru>
2024-12-25 22:06:23 +0300

    валид

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-function openDrawer() {
-  let checked = document.querySelector("input[type=checkbox]").checked;
-  let drawer = document.getElementById("menu-drawer");
-  drawer.setAttribute("opened", checked);
-}
+// function openDrawer() {
+//   let checked = document.querySelector("input[type=checkbox]").checked;
+//   let drawer = document.getElementById("menu-drawer");
+//   drawer.setAttribute("opened", checked);
+// }
 
 document
   .querySelector(".contact-form")
